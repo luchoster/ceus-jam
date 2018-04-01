85bf4b84b87b613c5f4db596e6ecc42aaaa1c758
Lucho <luchoster@gmail.com>
2018-04-01 14:19:14 -0700

    Portfolio page done

--- src/templates/portfolio-page.js
+++ src/templates/portfolio-page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { mapIndexed, createMarkup, notNilOrEmpty } from '../lib/helpers'
 import SqBg from '../assets/imgs/sq-pattern.png'
 
-export const PortfolioPageTemplate = ({ service }) => {
+export const PortfolioPageTemplate = ({ client }) => {
   return (
     <React.Fragment>
       <section
@@ -18,6 +18,26 @@ export const PortfolioPageTemplate = ({ service }) => {
           <h2>Our Portfolio</h2>
         </div>
       </section>
+      <section className="panel panel--white">
+        <div className="inner">
+          <section className="features">
+            {mapIndexed((item, index) => (
+              <article>
+                {item.link ? (
+                  <a href={item.link} target="_blank" className="image">
+                    <img src={item.image} alt={item.name} />
+                  </a>
+                ) : (
+                  <div className="image">
+                    <img src={item.image} alt={item.name} />
+                  </div>
+                )}
+                <h3 className="major">{item.name}</h3>
+              </article>
+            ))(client)}
+          </section>
+        </div>
+      </section>
     </React.Fragment>
   )
 }
@@ -25,7 +45,7 @@ export const PortfolioPageTemplate = ({ service }) => {
 const PortfolioPage = ({ data }) => {
   const { markdownRemark: post } = data
 
-  return <PortfolioPageTemplate service={post.frontmatter.service} />
+  return <PortfolioPageTemplate client={post.frontmatter.client} />
 }
 
 export default PortfolioPage
@@ -35,6 +55,11 @@ export const portfolioPageQuery = graphql`
     markdownRemark(id: { eq: $id }) {
       frontmatter {
         templateKey
+        client {
+          name
+          image
+          link
+        }
       }
     }
   }
